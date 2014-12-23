1d4ffb7077269e1a9034caf95075ec3a286970d8
Jody Clements <jody@neomorphic.co.uk>
2014-12-23 15:27:24 -0500

    Adds basic pages with routing from react-router
    This is a very basic skeleton of the app with routing implemented via
    the node module react-router.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,55 @@
-var React = require('react');
+var React = require('react'),
+ Router = require('react-router'),
+ Route = Router.Route,
+ NotFoundRoute = Router.NotFoundRoute,
+ DefaultRoute = Router.DefaultRoute,
+ Link = Router.Link,
+ RouteHandler = Router.RouteHandler;
 
-console.log(React);
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <header>
+          <ul>
+            <li><Link to="app">Home</Link></li>
+          </ul>
+        </header>
+
+        {/* this is the important part */}
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+var TileMap = React.createClass({
+  mixins: [Router.State],
+  render: function () {
+    return (
+      <div>
+        <h1>Tile map</h1>
+        <div>{this.getParams().uuid}</div>
+      </div>
+    );
+  }
+});
+
+var Home = React.createClass({
+  render: function () {
+    return (
+      <div>Home</div>
+    );
+  }
+});
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="uuid" path="uuid/:uuid" handler={TileMap}/>
+    <DefaultRoute handler={Home}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
