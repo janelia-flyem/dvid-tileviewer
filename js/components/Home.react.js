375dd057f90a6742c5ebb3fdaf6754c1013c0078
Jody Clements <jody@neomorphic.co.uk>
2014-12-23 17:33:59 -0500

    Adds a hard coded initial state for uuids
    This will eventually be replaced with an ajax call to the api server.

--- js/components/Home.react.js
+++ js/components/Home.react.js
@@ -1,9 +1,35 @@
 var React = require('react');
 
 var Home = React.createClass({
+  getInitialState: function() {
+    return {
+      uuids: [
+        '134c8f09429311e4a83dc81f66eb987e',
+        '2b6c789448c911e4ad8cc81f66eb987e'
+      ],
+      text: 'UUID list'
+    };
+  },
   render: function () {
     return (
-      <div>Home</div>
+      <div>
+        <h1>Home</h1>
+        <p>{this.state.text}</p>
+        <UUIDList items={this.state.uuids}/>
+      </div>
+    );
+  }
+});
+
+var UUIDList = React.createClass({
+  render: function() {
+    var renderItem = function(uuid) {
+      return (
+        <li>{uuid}</li>
+      );
+    };
+    return (
+      <ul>{this.props.items.map(renderItem)}</ul>
     );
   }
 });
