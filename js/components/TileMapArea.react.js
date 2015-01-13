04174505b11a2979ff75c04ea19f9342a0d58cd0
Jody Clements <jody@neomorphic.co.uk>
2015-01-13 09:15:44 -0500

    Replaces hard coded values with API calls.
    This replaces the hard coded dimensions of the stack with calls to the
    API to generate the information required.

--- js/components/TileMapArea.react.js
+++ js/components/TileMapArea.react.js
@@ -13,61 +13,72 @@ var TileMapArea = React.createClass({
       // set the variables for the tile viewer based on data fetched from the server
       var url = config.baseUrl();
 
-      $.get(config.datatypeInfoUrl(uuid, dataname), function (data) {
-        console.log(data);
-        var maxLevel = Object.keys(data.Extended.Levels).length - 1;
-        var volumeWidth = {
-            'xy': 6445,
-            'xz': 6445,
-            'yz': 6642
-        };
-        var volumeHeight = {
-            'xy': 6642,
-            'xz': 8089,
-            'yz': 8089
-        }
-        var volumeDepth = {
-            'xy': 8089,
-            'xz': 6642,
-            'yz': 6445
-        }
+      $.when($.ajax(config.datatypeInfoUrl(uuid, dataname)), $.ajax(config.datatypeInfoUrl(uuid, 'grayscale')))
+        .done(function(tileRequest, grayscaleRequest) {
+          var tileData = tileRequest[0],
+            gScaleData = grayscaleRequest[0],
+            maxPoint   = gScaleData.Extended.MaxPoint,
+            minPoint   = gScaleData.Extended.MinPoint,
+            dx        = maxPoint[0] - minPoint[0],
+            dy        = maxPoint[1] - minPoint[1],
+            dz        = maxPoint[2] - minPoint[2];
 
-        viewer = {
-          nmPerPixel: 10,
-          tileSource: {
-            height:    volumeHeight[slice],
-            width:     volumeWidth[slice],
-            tileSize:  data.Extended.Levels[0].TileSize[0],
-            minLevel:  0,
-            maxLevel:  maxLevel,
-            minZ:      0,
-            maxZ:      volumeDepth[slice]-1,
-            // getTileAtPoint: function(level, point) { Add offset to compute tiles }
-            getTileUrl: function xyTileURL(level, x, y, z) {
-              return url + "/api/node/" + uuid + "/" + dataname + "/tile/" + slice + "/" + (maxLevel-level) + "/" + x + "_" + y + "_" + z;
+          var maxLevel = Object.keys(tileData.Extended.Levels).length - 1;
+
+          var volumeWidth = {
+            'xy': dx,
+            'xz': dx,
+            'yz': dy,
+          };
+
+          var volumeHeight = {
+            'xy':  dy,
+            'xz':  dz,
+            'yz':  dz
+          };
+
+          var volumeDepth = {
+            'xy':  dz,
+            'xz':  dy,
+            'yz':  dx
+          };
+
+          viewer = {
+            nmPerPixel: 10,
+            tileSource: {
+              height:    volumeHeight[slice],
+              width:     volumeWidth[slice],
+              tileSize:  tileData.Extended.Levels[0].TileSize[0],
+              minLevel:  0,
+              maxLevel:  maxLevel,
+              minZ:      0,
+              maxZ:      volumeDepth[slice]-1,
+              // getTileAtPoint: function(level, point) { Add offset to compute tiles }
+              getTileUrl: function xyTileURL(level, x, y, z) {
+                return url + "/api/node/" + uuid + "/" + dataname + "/tile/" + slice + "/" + (maxLevel-level) + "/" + x + "_" + y + "_" + z;
+              }
             }
-          }
-        };
-        viewer.xy = OpenSeadragon({
-          id:                 "viewer",
-          prefixUrl:          "/js/openseadragon/images/",
-          navigatorSizeRatio: 0.25,
-          wrapHorizontal:     false,
-          maxZoomPixelRatio:  5.0,
-          showNavigator:      true,
-          tileSources:        viewer.tileSource
+          };
+          viewer.xy = OpenSeadragon({
+            id:                 "viewer",
+            prefixUrl:          "/js/openseadragon/images/",
+            navigatorSizeRatio: 0.25,
+            wrapHorizontal:     false,
+            maxZoomPixelRatio:  5.0,
+            showNavigator:      true,
+            tileSources:        viewer.tileSource
+          });
+          viewer.xy.scalebar({
+            pixelsPerMeter: 1000000000/viewer.nmPerPixel,
+            fontColor:      "yellow",
+            color:          "yellow"
+          });
+
         });
-        viewer.xy.scalebar({
-          pixelsPerMeter: 1000000000/viewer.nmPerPixel,
-          fontColor:      "yellow",
-          color:          "yellow"
-        });
-      });
     }
   },
 
   componentWillUnmount: function() {
-    console.log(viewer);
     if (viewer && viewer.xy) {
       viewer.xy.destroy();
       viewer = null;
